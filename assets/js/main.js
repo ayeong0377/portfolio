6bc029570c62132c8bb7b4f84553afc7b399de14
ayeong0377 <ayeong0377@gmail.com>
2026-04-20 15:34:41 +0900

    about페이지 수정, project 슬라이드로 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
       //   kv-title 이동
       const box = document.querySelector('.main-title-box');
 
-        setTimeout(() => {
-            box.classList.add('active');
-        }, 300)
+      setTimeout(() => {
+        box.classList.add('active');
+      }, 300);
+
+      const projectSwiper = new Swiper('.project-swiper', {
+        slidesPerView: 1,
+        spaceBetween: 16,
+        navigation: {
+          prevEl: '.project-button-prev',
+          nextEl: '.project-button-next',
+        },
+        breakpoints: {
+          769: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        },
+      });
     });
 
